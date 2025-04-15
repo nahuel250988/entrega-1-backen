c46275a0458f349e8f20d36bd2e80a4baa8e4107
nahuel rodridguez <nahuel251204@gmail.com>
2025-04-14 21:27:07 -0300

    ver

--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
 import express from 'express';
-import ProductManager from './ProductManager.js';
+import ProductManager from './ProductsManager.js';
+import CartManager from './CartsManager.js';
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const productManager = new ProductManager('./src/products.json');
+const cartManager = new CartManager('./src/carts.json');
 
 
 app.use(express.json());
 
 
-app.post('/', async (req, res) => {
+app.post('/products', async (req, res) => {
     const { title, description, code, price, status, stock, category, thumbnails } = req.body;
 
-    
     if (!title || !description || !code || !price || !status || !stock || !category || !Array.isArray(thumbnails)) {
         return res.status(400).json({ error: 'Faltan campos requeridos' });
     }
@@ -27,12 +28,48 @@ app.post('/', async (req, res) => {
             status,
             stock,
             category,
-            thumbnails
+            thumbnails,
         });
 
         res.status(201).json(newProduct); 
     } catch (error) {
         res.status(500).json({ error: 'Error al crear el producto' });
+    }
+});
+
+
+app.post('/cart', async (req, res) => {
+    try {
+        const newCart = await cartManager.createCart();
+        res.status(201).json(newCart); 
+    } catch (error) {
+        res.status(500).json({ error: 'Error al crear el carrito' });
+    }
+});
+
+
+app.post('/cart/:cartId/product/:productId', async (req, res) => {
+    const { cartId, productId } = req.params;
+    const { quantity } = req.body;
+
+    if (!quantity || quantity <= 0) {
+        return res.status(400).json({ error: 'Cantidad inválida' });
+    }
+
+    try {
+        const product = await productManager.getProductById(productId);
+        if (!product) {
+            return res.status(404).json({ error: 'Producto no encontrado' });
+        }
+
+        const updatedCart = await cartManager.addProductToCart(cartId, productId, quantity);
+        if (updatedCart) {
+            res.status(200).json(updatedCart); 
+        } else {
+            res.status(404).json({ error: 'Carrito no encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error al agregar el producto al carrito' });
     }
 });
 
